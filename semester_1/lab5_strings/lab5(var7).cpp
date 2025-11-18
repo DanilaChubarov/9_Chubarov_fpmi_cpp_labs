1236759312ee0086876b9c7afe5976c10639900d
DanilaChubarov <thubarov21@gmail.com>
2025-11-18 14:52:32 +0300

    Refactor string processing and digit checking functions

--- semester_1/lab5_strings/lab5(var7).cpp
+++ semester_1/lab5_strings/lab5(var7).cpp
@@ -1,78 +1,83 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include <cctype>
-
-bool isDigitsOnly(const std::string& s) {
-    for (char c : s) {
-        if (!isdigit(c)) return false;
-    }
-    return !s.empty();
+#include<cctype>
+#include<vector>
+bool IsDigitsOnly(std::string word)
+{
+	for (char symbol : word)
+	{
+		if (!isdigit(symbol))
+		{
+			return false;
+		}
+	}
+	return true;
+	
 }
+int FindingBiggestNumber(std::vector<int> numbers_int)
+{
+	int big = numbers_int[0];
+	for (int i = 1; i < (int) numbers_int.size(); i++)
+	{
+		if (big < numbers_int[i])
+		{
+			big = numbers_int[i];
+		}
+	}
+	return big;
+}
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	std::string text;
+	std::cout << "Введите строку\n";
+	getline(std::cin, text);
+	std::vector<std::string> words;
+	std::vector<std::string> numbers_str;
+	std::vector<int> numbers_int;
+	std::string::size_type beg_index = 0;
+	std::string::size_type end_index = text.find_first_of(" ", beg_index);
+	while (beg_index != std::string::npos) {
 
-int main() {
-    std::string line;
-    std::getline(std::cin, line);
+		std::string word = text.substr(beg_index, end_index - beg_index);
+		if (!word.empty()) {
+			words.push_back(word);
+		}
 
-    std::vector<std::string> words;
-    std::string word;
+		beg_index = text.find_first_not_of(" ", end_index);
+		end_index = text.find_first_of(" ", end_index + 1);
+	}
+	for (int i=0; i <(int) words.size(); i++)
+	{
+		if (IsDigitsOnly(words[i]))
+		{
+			numbers_str.push_back(words[i]);
+		}
+	}
+	for (int i = 0; i < (int)numbers_str.size(); i++)
+	{
+		numbers_int.push_back(std::stoi(numbers_str[i]));
+	}
+	int big = FindingBiggestNumber(numbers_int);
+	std::string biggest_number=std::to_string(big);
+	std::string::size_type biggest_number_start_index = text.find(biggest_number);
+	std::string::size_type biggest_number_end_index = text.find_first_of(" ", biggest_number_start_index);
+	text.erase(biggest_number_start_index, (biggest_number_end_index-biggest_number_start_index));
+	biggest_number = biggest_number + " ";
+	text.insert(0,biggest_number);
+	std::string::size_type biggest_number_index= text.find_first_of(" ", beg_index);
 
-    for (size_t i = 0; i < line.size(); i++) {
-        if (line[i] != ' ') {
-            word += line[i];
-        }
-        else if (!word.empty()) {
-            words.push_back(word);
-            word.clear();
-        }
-    }
-    if (!word.empty()) {
-        words.push_back(word);
-    }
+	
 
-    std::vector<int> digitIndices;
 
-    for (int i = 0; i < (int)words.size(); i++) {
-        if (isDigitsOnly(words[i])) {
-            digitIndices.push_back(i);
-        }
-    }
+	std::cout << text << std::endl;
+	
 
-    if (digitIndices.empty()) {
-        std::cout << line;
-        return 0;
-    }
 
-    long long maxVal = -1;
-    std::vector<int> maxima;
-    for (int idx : digitIndices) {
-        long long val = stoll(words[idx]);
-        if (val > maxVal) {
-            maxVal = val;
-            maxima.clear();
-            maxima.push_back(idx);
-        }
-        else if (val == maxVal) {
-            maxima.push_back(idx);
-        }
-    }
 
-    int chosenIndex;
-    if (maxima.size() == 1) {
-        chosenIndex = maxima[0];
-    }
-    else {
-        chosenIndex = maxima[maxima.size() - 2];
-    }
 
-    std::string chosenWord = words[chosenIndex];
-    words.erase(words.begin() + chosenIndex);
-    words.insert(words.begin(), chosenWord);
 
-    for (size_t i = 0; i < words.size(); i++) {
-        std::cout << words[i];
-        if (i + 1 < words.size())std::cout << " ";
-    }
 
-    return 0;
+
+	return 0;
 }
