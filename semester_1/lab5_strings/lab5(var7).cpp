fbe303437401675bb26ef0ee70e58fa6edbfca6b
DanilaChubarov <thubarov21@gmail.com>
2025-12-10 11:44:20 +0300

    Update lab5(var7).cpp

--- semester_1/lab5_strings/lab5(var7).cpp
+++ semester_1/lab5_strings/lab5(var7).cpp
@@ -26,19 +26,12 @@ int FindingBiggestNumber(std::vector<int> numbers_int)
 	}
 	return big;
 }
-int main()
-{
-	setlocale(LC_ALL, "rus");
-	std::string text;
-	std::cout << "Введите строку\n";
-	getline(std::cin, text);
+std::vector<std::string>  ParsingWords (std::string& text)
+{ 
 	std::vector<std::string> words;
-	std::vector<std::string> numbers_str;
-	std::vector<int> numbers_int;
-	std::string::size_type beg_index = 0;
-	std::string::size_type end_index = text.find_first_of(" ", beg_index);
+    std::string::size_type beg_index = 0;
+    std::string::size_type end_index = text.find_first_of(" ", beg_index);
 	while (beg_index != std::string::npos) {
-
 		std::string word = text.substr(beg_index, end_index - beg_index);
 		if (!word.empty()) {
 			words.push_back(word);
@@ -47,37 +40,64 @@ int main()
 		beg_index = text.find_first_not_of(" ", end_index);
 		end_index = text.find_first_of(" ", end_index + 1);
 	}
-	for (int i=0; i <(int) words.size(); i++)
+	return words;
+}
+void FillNumbersStr(const std::vector<std::string>& words, std::vector<std::string>& numbers_str)
+{
+	for (int i = 0; i < (int)words.size(); i++)
 	{
 		if (IsDigitsOnly(words[i]))
 		{
 			numbers_str.push_back(words[i]);
 		}
 	}
+}
+void FillNumbersInt(const std::vector<std::string>& numbers_str, std::vector<int>& numbers_int)
+{
 	for (int i = 0; i < (int)numbers_str.size(); i++)
 	{
 		numbers_int.push_back(std::stoi(numbers_str[i]));
 	}
+}
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	std::string text;
+	std::cout << "Введите строку\n";
+	getline(std::cin, text);
+	if (text.empty())
+	{
+		std::cerr << "Вы ничего не ввели!" << std::endl;
+		return 0;
+	}
+
+	std::vector<std::string> words;
+	words = ParsingWords(text);
+
+	std::vector<std::string> numbers_str;
+	FillNumbersStr(words, numbers_str);
+	if (numbers_str.empty())
+	{
+		std::cerr << "В строке нет чисел!" << std::endl;
+		return 0;
+	}
+	if (numbers_str.size()==1)
+	{
+		std::cout << text << std::endl;
+		return 0;
+	}
+
+	std::vector<int> numbers_int;
+	FillNumbersInt(numbers_str, numbers_int);
+
 	int big = FindingBiggestNumber(numbers_int);
 	std::string biggest_number=std::to_string(big);
 	std::string::size_type biggest_number_start_index = text.find(biggest_number);
-	std::string::size_type biggest_number_end_index = text.find_first_of(" ", biggest_number_start_index);
+	std::string::size_type biggest_number_end_index = text.find_first_of(" ", biggest_number_start_index)+1;
 	text.erase(biggest_number_start_index, (biggest_number_end_index-biggest_number_start_index));
 	biggest_number = biggest_number + " ";
 	text.insert(0,biggest_number);
-	std::string::size_type biggest_number_index= text.find_first_of(" ", beg_index);
-
-	
-
-
 	std::cout << text << std::endl;
-	
-
-
-
-
-
-
-
 	return 0;
 }
