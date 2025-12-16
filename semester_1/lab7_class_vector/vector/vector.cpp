bb7bd04072c7862f893232a1ad37925651f97cea
DanilaChubarov <thubarov21@gmail.com>
2025-12-16 21:32:24 +0300

    Fix include directives and add main function

--- semester_1/lab7_class_vector/vector/vector.cpp
+++ semester_1/lab7_class_vector/vector/vector.cpp
@@ -1,3 +1,16 @@
-#include <vector.h>
+#include "vector.h"
+#include <iostream>
+int main()
+{
+	Vector array(3);
+	Vector massiv{ 1,34 ,42 };
+	Vector array_copy(massiv);
+	for (size_t i = 0; i < 3; i++)
+	{
+		std::cout << array_copy[i] << " ";
+	}
+		
+	
 
-// your code goes here
+	return 0;
+}
