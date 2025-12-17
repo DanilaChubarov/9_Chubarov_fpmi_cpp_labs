35e59310cf6cc98c7bb5ad98f967009376a9aac4
DanilaChubarov <thubarov21@gmail.com>
2025-12-17 20:26:13 +0300

    Update vector.cpp

--- semester_1/lab7_class_vector/vector/vector.cpp
+++ semester_1/lab7_class_vector/vector/vector.cpp
@@ -2,13 +2,20 @@
 #include <iostream>
 int main()
 {
-	Vector array(3);
+	Vector array(10);
 	Vector massiv{ 1,34 ,42 };
-	Vector array_copy(massiv);
-	for (size_t i = 0; i < 3; i++)
-	{
-		std::cout << array_copy[i] << " ";
-	}
+	massiv.PopBack();
+	massiv.Reserve(16);
+	std::cout<<massiv;
+
+
+
+
+
+	
+	
+	
+	
 		
 	
 
