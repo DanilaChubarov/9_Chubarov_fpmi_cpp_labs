6c46a6ba13b2c718f648344081716a8bf0265aba
DanilaChubarov <thubarov21@gmail.com>
2025-12-17 20:27:01 +0300

    Update vector.h

--- semester_1/lab7_class_vector/vector/vector.h
+++ semester_1/lab7_class_vector/vector/vector.h
@@ -1,19 +1,24 @@
 #pragma once
 #include <initializer_list>
+#include <utility>
+#include <algorithm>
+#include <ostream>
+
 class Vector
 {
 private:
 	size_t size_ = 0;
 	int* data_ = nullptr;
-	int capacity_ = 0;
+	size_t capacity_ = 0;
 public:
+	
 	Vector()
 	{
 		size_ = 0;
 		data_ = nullptr;
 		capacity_ = 0;
 	}
-	Vector(int n)
+	Vector(size_t n)
 	{
 		size_ = n;
 		data_ = new int[n];
@@ -21,7 +26,7 @@ public:
 		{
 			data_[i] = 0;
 		}
-		capacity_ = (sizeof(int) * n);
+		capacity_ = n;
 	}	
 	Vector(std::initializer_list<int> list)
 	{
@@ -32,17 +37,40 @@ public:
 			data_[i] = number;
 			++i;
 		}
-		capacity_ = (sizeof(int) * list.size());
+		capacity_ =  list.size();
 	}
 	Vector(const Vector& other)
 	{
 		size_ = other.size_;
-		data_ = new int[other.size_];
+		data_ = new int[other.capacity_];
 		for (size_t i = 0; i < other.size_; ++i)
 		{
 			data_[i] = other.data_[i];
 		}
-		capacity_= (sizeof(int) * other.size_);
+		capacity_=  other.capacity_;
+	}
+	~Vector()
+	{
+		delete[] data_;
+	}
+	Vector& operator=(const Vector& other)
+	{
+		if (this == &other) 
+		{
+			return *this;
+		}
+		size_ = other.size_;
+		delete[] data_;
+		data_ = new int[other.capacity_];
+		std::copy(other.data_, other.data_ + size_, data_);
+		capacity_ =  other.capacity_;
+		return *this;
+	}
+	void Swap(Vector& other) 
+	{
+		std::swap(size_, other.size_);
+		std::swap(data_, other.data_);
+		std::swap(capacity_, other.capacity_);
 	}
 	int& operator[](size_t index) 
 	{
@@ -52,4 +80,89 @@ public:
 	{
 		return data_[index];
 	}
+	int Size()
+	{
+		return size_;
+	}
+	int Capacity()
+	{
+		return capacity_;
+	}
+	void PushBack(int number)
+	{
+		if ((size_ + 1) <= capacity_)
+		{
+			data_[size_] = number;
+			size_ = size_ + 1;
+
+		}
+		else
+		{
+			size_t size_new_ = (size_+1);
+			size_t capacity_new_;
+			if (capacity_ == 0) 
+			{
+				capacity_new_ = 1;
+			}
+			else 
+			{
+				capacity_new_ = capacity_ * 2;
+			}
+			int* data_new_ = new int[capacity_new_];
+			for (size_t i = 0; i < size_; ++i)
+			{
+				data_new_[i] = data_[i];
+			}
+			data_new_[size_] = number;
+			delete[] data_;
+			size_ = size_new_;
+			capacity_ = capacity_new_;
+			data_ = data_new_;
+		}
+	}
+	void PopBack()
+	{
+		if (size_ == 0)
+		{
+			return;
+		}
+		size_--;
+	}
+	void Clear()
+	{
+		size_ = 0;
+	}
+	void Reserve(size_t amount)
+	{
+		if (capacity_ >= amount)
+		{
+			return;
+		}
+    size_t capacity_new_ = amount;
+	int* data_new_ = new int[capacity_new_];
+	for (size_t i = 0; i < size_; ++i)
+	{
+		data_new_[i] = data_[i];
+	}
+	delete[] data_;
+	capacity_ = capacity_new_;
+	data_ = data_new_;
+		
+	}
+	
 };
+std::ostream& operator<<(std::ostream& cout,  Vector& vector)
+{
+	cout << "[";
+	if (vector.Size() == 0)
+	{
+		cout << "]";
+	}
+	for (size_t i = 0; i < vector.Size()-1; ++i)
+	{
+		cout << vector[i] << ", ";
+	}
+	cout << vector[vector.Size() - 1];
+	cout << "]\n";
+	return cout;
+}
